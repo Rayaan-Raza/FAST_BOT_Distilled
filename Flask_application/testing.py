94a37211a5560087d9648cad19ddecaa5076532e
Rayaan Raza <110607824+Rayaan-Raza@users.noreply.github.com>
2025-07-02 22:31:00 +0500

    Update testing.py

--- Flask_application/testing.py
+++ Flask_application/testing.py
@@ -1,8 +1,1 @@
-import requests
 
-API_URL = "https://api-inference.huggingface.co/models/Phe-nix/fast-bot-bert"
-headers = {"Authorization": "Bearer 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"}
-
-response = requests.post(API_URL, headers=headers, json={"inputs": "hello"})
-print(response.status_code)
-print(response.text)
