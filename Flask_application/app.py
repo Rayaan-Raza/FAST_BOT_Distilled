96085513d4c819d1eab2eb1ce11e80148a694507
Rayaan Raza <110607824+Rayaan-Raza@users.noreply.github.com>
2025-07-02 20:45:20 +0500

    Update app.py

--- Flask_application/app.py
+++ Flask_application/app.py
@@ -26,4 +26,5 @@ def handle_message():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',debug=True)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port, debug=False)
